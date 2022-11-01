78fe03f39e5f6915ffeef2e560b529cd6067013b
Aaron Liu <aaron155658@gmail.com>
2022-11-01 17:35:38 -0400

    add css and bootstrap

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,5 +1,8 @@
 import "./form";
 import "./submit";
+import { Tooltip, Toast, Popover } from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import Logo from "../images/logo.png";
 import Bear from "../images/bear.png";
 import Dog from "../images/dog.png";
@@ -9,3 +12,6 @@ window.addEventListener("load", function () {
   document.getElementById("bearThumbnail").src = Bear;
   document.getElementById("dogThumbnail").src = Dog;
 });
+
+// Import CSS files
+import "../css/index.css";
