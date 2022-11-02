479ca589303b04c132c0fa5afd03a00a43226579
Aaron Liu <aaron155658@gmail.com>
2022-11-01 22:41:03 -0400

    create card

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,5 +1,4 @@
-import "./form";
-import "./submit";
+import { toggleForm, clearForm } from "./form";
 import { Tooltip, Toast, Popover } from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -7,13 +6,49 @@ import Logo from "../images/logo.png";
 import Bear from "../images/bear.png";
 import Dog from "../images/dog.png";
 
-import { initdb } from "./database";
+import { initdb, getDb, postDb } from "./database";
+import { fetchCards } from "./card";
 
 window.addEventListener("load", function () {
   initdb();
+  fetchCards();
   document.getElementById("logo").src = Logo;
   document.getElementById("bearThumbnail").src = Bear;
   document.getElementById("dogThumbnail").src = Dog;
+});
+
+const form = document.getElementById("formToggle");
+const newContactButton = document.getElementById("new-contact");
+let submitBtnToUpdate = false;
+let profileId;
+
+newContactButton.addEventListener("click", (event) => {
+  toggleForm();
+});
+
+form.addEventListener("submit", (event) => {
+  // Handle data
+  event.preventDefault();
+  let name = document.getElementById("name").value;
+  let phone = document.getElementById("phone").value;
+  let email = document.getElementById("email").value;
+  let profile = document.querySelector('input[type="radio"]:checked').value;
+
+  // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+  if (submitBtnToUpdate == false) {
+    postDb(name, email, phone, profile);
+  } else {
+    fetchCards();
+    // Toggles the submit button back to POST functionality
+    submitBtnToUpdate = false;
+  }
+
+  // Clear form
+  clearForm();
+  // Toggle form
+  toggleForm();
+  // Reload the DOM
+  fetchCards();
 });
 
 // Import CSS files
