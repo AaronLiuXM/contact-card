510f08b571f628648df1e294dc6f1d7b14be1a51
Aaron <aaron155658@gmail.com>
2022-11-03 11:44:38 -0400

    add manifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const WorkboxPlugin = require("workbox-webpack-plugin");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: "./src/js/index.js",
@@ -13,7 +13,10 @@ module.exports = {
       template: "./index.html",
       title: "Webpack Plugin",
     }),
-    new WorkboxPlugin.GenerateSW(),
+    new InjectManifest({
+      swSrc: "./src/sw.js",
+      swDest: "service-worker.js",
+    }),
   ],
   module: {
     rules: [
